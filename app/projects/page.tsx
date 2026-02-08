8f4d85f52b49e35a46ecbff9b1b1b4ea6a2ba83e
devyinka <217584194+devyinka@users.noreply.github.com>
2026-02-08 04:28:43 +0100

    Reduce gap between project cards

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,7 +5,7 @@ import { Project } from "../components/Project";
 export default function Projects() {
   return (
     <section className="p-4 pb-12 max-w-6xl m-auto overflow-hidden 2xl:overflow-visible">
-      <div className="flex flex-col gap-20">
+      <div className="flex flex-col gap-8">
         {ProjectList.map((project: ProjectType) => (
           <Project
             key={`project-item-${project.title}`}
